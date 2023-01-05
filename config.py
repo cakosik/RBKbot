e38d3f52e4a1d66eb33e61e76ba6086972528247
cakosik <115614081+cakosik@users.noreply.github.com>
2023-01-05 20:13:15 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+
+tokens = ['5983585519:AAGwXXvjSVkcKXtzBEDCq1CvxcguX5gZyKA','5304306242:AAGIJjSQfEKHJalM0qkBGr_kdDiFxX76R2w'] #  1 - @qwegamebot | 2 - @testbottttttttttttbotssetrobot
+token = tokens[0]
+QiwiP2P_token = 'eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6ImdibWRrNS0wMCIsInVzZXJfaWQiOiI3OTI4MTI2MjE2NSIsInNlY3JldCI6ImQwNjU2YWUwY2VkNmFjZDg1MTZhYjlkYTk5ZjViZjk1N2I2NmE1NGY4OGMzZjY5OWNiMThkNjE5OWY2OTNjYTEifX0='
+
+owner_id = 1887634547
+
+full_bot_name = 'QWE'
+bot_name = 'qwegamebot'
+channel_name = 'qwechannel'
+chat_name = 'qwe_chat_two'
+
+start_money = 10000
+donat_money = 0
+cash_family = 5
+
+
+
+owner = '<a href="t.me/haeshka_qwe_perehodnik/">Хаешка</a>'
+channel = '<a href="t.me/qwechannel/">QWE CHANNEL | Канал</a>'
+chat = '<a href="t.me/qwechat">QWE | Игровой чат #1</a>'
+chat2 = '<a href="t.me/qwe_chat_two">QWE | Игровой чат #2</a>'
+chat_id = -1001789828191 #  @qwechat  -1001537031068 | QWE ADMIN CHAT  -1001535128629 | @qwe_chat_two -1001692908382
+admin_chat_id = '-1001535128629'
+
